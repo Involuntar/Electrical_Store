eade69f299656c5eb653a9b47bf7bc8aef000a79
Involuntar <ilya.nevolin.2004@mail.ru>
2025-05-24 17:49:37 +0500

    сделал фильтрацию по категории и цене с пагинацией

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Query
 from database import get_db
 from sqlalchemy.orm import Session
 import models as m
@@ -10,6 +10,16 @@ app=FastAPI()
 
 # Товары
 @app.get("/api/products", response_model=List[pyd.SchemeProduct])
-def get_products(db:Session=Depends(get_db)):
-    products = db.query(m.Product).all()
+def get_products(limit:None|int=Query(None), page:None|int=Query(1), category:None|int=Query(None), minPrice:None|float=Query(None), db:Session=Depends(get_db)):
+    products = db.query(m.Product)
+    if category:
+        products = products.filter(
+            m.Product.category_id == category
+        )
+    if minPrice:
+        products = products.filter(
+            m.Product.product_price >= minPrice
+        )
+    if limit:
+        products = products[(page - 1) * limit:page * limit]
     return products
